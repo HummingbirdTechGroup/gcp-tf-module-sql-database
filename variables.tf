953fd6f460fce6f5ba38a588c13e0b74ce7d8dec
Simon Elliott <33635580+sce81@users.noreply.github.com>
2019-05-10 13:21:23 +0100

    PLAT-423 (#1)
    Added Support for multiple user creation via count
    Removed public IP requirement

--- variables.tf
+++ variables.tf
@@ -1,26 +1,22 @@
 variable env                    {}
 variable name                   {}
+#variable region                 {}
 variable zone                   {}
 variable tier                   {}
 variable disk_size              {}
 variable disk_type              {}
 variable require_ssl            {}
 variable private_network        {}
-#variable ip_purpose             {}
 variable address_type           {}
 variable database_version       {}
-
-#variable prefix_length          {}
-#variable network                {}
-variable certificate            {}
-
 variable app_name               {} 
 variable team                   {} 
 variable cost_type              {} 
-
-variable sql_user_name          {}
-variable sql_user_host          {}
-variable sql_user_password      {}
-
 variable whitelisted_name       {}
 variable whitelisted_ip         {}
+
+
+variable sql_user_count         {default = 1}
+variable sql_user_name          {type = "list"}
+variable sql_user_host          {type = "list"}
+variable sql_user_password      {type = "list"}
