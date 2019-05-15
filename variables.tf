eb4ca4f10a0976e932d214a5f216973a6ba802d4
Simon Elliott <simon@hummingbirdtech.com>
2019-05-15 17:28:32 +0100

    merge hell

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 variable env                    {}
 variable name                   {}
-<<<<<<< HEAD
-variable region                 {}
-=======
 #variable region                 {}
->>>>>>> ea4f78e002bd92ac9791c2c58e189b04c67bdd64
 variable zone                   {}
 variable tier                   {}
 variable disk_size              {}
@@ -19,16 +15,8 @@ variable cost_type              {}
 variable whitelisted_name       {}
 variable whitelisted_ip         {}
 
-<<<<<<< HEAD
-
-
-variable sql_user_name          {}
-variable sql_user_host          {}
-variable sql_user_password      {}
-=======
 variable public_ip              {default = "false"}
 variable sql_user_count         {default = 1}
 variable sql_user_name          {type = "list"}
 variable sql_user_host          {type = "list"}
 variable sql_user_password      {type = "list"}
->>>>>>> ea4f78e002bd92ac9791c2c58e189b04c67bdd64
