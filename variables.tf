46e7f54b1a8760c9f836596def7f6c7e28459993
Simon Elliott <33635580+sce81@users.noreply.github.com>
2019-06-20 11:52:27 +0100

    PLAT-423 - Adds support for multiple users/passwords as list

--- variables.tf
+++ variables.tf
@@ -14,7 +14,6 @@ variable team                   {}
 variable cost_type              {} 
 #variable whitelisted_name       {}
 #variable whitelisted_ip         {}
-
 variable public_ip              {default = "false"}
 variable sql_user_count         {default = 1}
 variable sql_user_name          {type = "list"}
@@ -22,3 +21,4 @@ variable sql_user_host          {type = "list"}
 variable sql_user_password      {type = "list"}
 variable binary_log_enabled     {default = "true"}
 variable project                {default = "hummingbird-technologies"}
+
