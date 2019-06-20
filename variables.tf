717af10bce5a4acc529499c6e0eb9aa5ba995d30
Simon Elliott <simon@hummingbirdtech.com>
2019-06-20 11:54:56 +0100

    PLAT-498 - Terraform0.12 upgrade

--- variables.tf
+++ variables.tf
@@ -1,24 +1,70 @@
-variable env                    {}
-variable name                   {}
+variable "env" {
+}
+
+variable "name" {
+}
+
 #variable region                 {}
-variable zone                   {}
-variable tier                   {}
-variable disk_size              {}
-variable disk_type              {}
-variable require_ssl            {}
-variable private_network        {}
-variable address_type           {}
-variable database_version       {}
-variable app_name               {} 
-variable team                   {} 
-variable cost_type              {} 
+variable "zone" {
+}
+
+variable "tier" {
+}
+
+variable "disk_size" {
+}
+
+variable "disk_type" {
+}
+
+variable "require_ssl" {
+}
+
+variable "private_network" {
+}
+
+variable "address_type" {
+}
+
+variable "database_version" {
+}
+
+variable "app_name" {
+}
+
+variable "team" {
+}
+
+variable "cost_type" {
+}
+
 #variable whitelisted_name       {}
 #variable whitelisted_ip         {}
-variable public_ip              {default = "false"}
-variable sql_user_count         {default = 1}
-variable sql_user_name          {type = "list"}
-variable sql_user_host          {type = "list"}
-variable sql_user_password      {type = "list"}
-variable binary_log_enabled     {default = "true"}
-variable project                {default = "hummingbird-technologies"}
+variable "public_ip" {
+  default = "false"
+}
+
+variable "sql_user_count" {
+  default = 1
+}
+
+variable "sql_user_name" {
+  type = list(string)
+}
+
+variable "sql_user_host" {
+  type = list(string)
+}
+
+variable "sql_user_password" {
+  type = list(string)
+}
+
+variable "binary_log_enabled" {
+  default = "true"
+}
+
+variable "project" {
+  default = "hummingbird-technologies"
+}
 
